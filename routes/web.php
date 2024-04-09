86c05c4b69e2229707b1afb3f60865768a4bff08
ogutaku5 <shourinzi524@gmail.com>
2024-04-09 23:41:16 +0900

    カリキュラム8-5終了

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,6 @@ Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}', [PostController::class, 'show']);
 Route::get('/posts', [PostController::class, 'store']);
 Route::post('/posts', [PostController::class, 'store']);
+Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
+Route::put('/posts/{post}', [PostController::class, 'update']);
 
